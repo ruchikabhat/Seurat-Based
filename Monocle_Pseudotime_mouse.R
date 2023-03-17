68abe1957af14c81be76176a6a241a16bb0985b4
ruchikabhat <ruchikabhat31@gmail.com>
2023-03-17 11:03:00 +0530

    Update Monocle_Pseudotime_mouse.R

--- Monocle_Pseudotime_mouse.R
+++ Monocle_Pseudotime_mouse.R
@@ -3,75 +3,29 @@ library(SeuratDisk)
 library(SeuratData)
 library(ggplot2)
 library(patchwork)
-library('reticulate')
-library(Matrix)
-library(cowplot)
-library(ggplot2)
-library(patchwork)
 library(dplyr)
-library(harmony)
-library(patchwork)
 library(BiocManager)
-#library(multtest)
-#library(metap)
-#library(SingleR)
-library(EnhancedVolcano)
 memory.limit(size=7800000000000)
 library(RColorBrewer)
 n <- 30
 qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
 col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
 a<-DiscretePalette(n, palette = NULL)
-library(ggplot2)
 #BiocManager::install("monocle")
 library(monocle)
 
-######load the seurat object first ######
+######set working directory ######
 setwd ("D:/HPC_DATA_MOUSE_NEW COVID_April2022/Rrun")
-#mouse<-readRDS("Allmouse_updatedcelltypes_14June.rds") #just one version before newest
-mouse<-readRDS("mouseAnnotated_16thMarch2023.rds")     ##NEWEST ##
-FibroMono<-readRDS("Pseudotime_onlyFRCs.rds")
-Idents(mouse)<-"seurat_clusters"
-
-FRC<-subset(Frcd,idents= c('FRCs'), invert=FALSE)
-Idents(FRC)<-"seurat_clusters"
-cluster_letters <- Idents(object = mouse)
-names(cluster_letters) <- colnames(x = mouse)
-FRC <- AddMetaData(
-  object = mouse,
-  metadata = cluster_letters,
-  col.name = 'Clusters'
-)
-View(FRC@meta.data)
-Idents(FRC)<-'Clusters'
-FRC <- RenameIdents(FRC, '0' = 'Ccl19low Type1', '1' = 'Cd34+ Type2', '3'='Inmt+', '5'='Nr4a1+','7'='Ccl19high','4'='Ccl19low Type2','17'='Cd34+ Type1','6'='Enthothelial Cells','18'='NerveCells','9'='SmoothMuscle','12'='Pericytes','2'='CapEC1','8'='fLECs','10'='Art','11'='cLECs','13'='TrEC+HEV','14'='CapEC2','15'='Macro/Ptx3 LECs','16'='Cd45+Bcells','19'='Cd45+Tcells','20'='CapIfn','21'='Cd45+Bcells')
-cluster_letters <- Idents(object = FRC)
-names(cluster_letters) <- colnames(x = FRC)
-FRC <- AddMetaData(
-  object = FRC,
-  metadata = cluster_letters,
-  col.name = 'Clusters'
-)
-Idents(Frcd)<-'Condition'
-Ad<-subset(Frcd,idents= c('Adult'), invert=FALSE)
-Od<-subset(Frcd,idents= c('Old'), invert=FALSE)
-saveRDS(FRC, "mouseAnnotated_16thMarch2023.rds")
-View(FRC@meta.data)
+######load the seurat object first ######
 #seuratobject<-readRDS(YOURPATH/SEURATRDSfile)
-FRC<-Od
-Idents(FRC)<-"Clusters"
+
+mouse<-readRDS("All.rds") 
+
 data <- as(as.matrix(FRC@assays$RNA@data), 'sparseMatrix')
 pd <- new('AnnotatedDataFrame', data = FRC@meta.data)
 fData <- data.frame(gene_short_name = row.names(data), row.names = row.names(data))# save list of gene names
 fd <- new('AnnotatedDataFrame', data = fData)
 
-#install.packages("vctrs")
-
-#cds <- new_cell_data_set(data,                         cell_metadata = pd                         gene_metadata = fd)
-
-#FibroMono <- newCellDataSet(data,
-         #                   phenoData  = pd,
-          #                  featureData = fd,expressionFamily = negbinomial.size())
 FibroMono <- newCellDataSet(data,
                             phenoData  = pd,
                             featureData = fd,expressionFamily = uninormal())
@@ -106,7 +60,7 @@ FibroMono <- orderCells(FibroMono)
 
 
 plot_cell_trajectory(FibroMono, 
-                     color_by = "updated_subtypes",
+                     color_by = "Celltypes",
                      theta = -15,
                      show_branch_points = TRUE,show_backbone = TRUE,
                      backbone_color = "black",
@@ -114,6 +68,7 @@ plot_cell_trajectory(FibroMono,
                      cell_size = 1.5, values=a) + scale_color_manual(values = a, name = "Only FRCs Pseudotime")+ theme(legend.position = "top")
 
 saveRDS(FibroMono, "Pseudotime.rds")
+
 # now onwards to pseudotemporal plots
 genex <- c("Inmt")
 
@@ -148,22 +103,8 @@ diff_test_res <- differentialGeneTest(FibroMono[1:100,],
                                       fullModelFormulaStr = "~updated_subtypes")
 ordering_genes <- row.names (subset(diff_test_res, qval < 0.01))
 
-########### NEW PART FROM ANOTHER SCRIPT ##############
-var.genes <- VariableFeatures(mouse)
-FibroMono <- setOrderingFilter(FibroMono, var.genes)
-p1 <- plot_ordering_genes(FibroMono)
-
-disp_table <- dispersionTable(FibroMono)
-disp.genes <- subset(disp_table, mean_expression >= 0.1 & dispersion_empirical >= 1 * dispersion_fit)$gene_id
-FibroMono <- setOrderingFilter(FibroMono, disp.genes)
-p2 <- plot_ordering_genes(FibroMono)
-
-p1
-p2
 #######################################
-FibroMono<- setOrderingFilter(FibroMono, ordering_genes)
-FibroMono <- reduceDimension(FibroMono, max_components = 2,method = 'DDRTree', auto_param_selection = F)
-FibroMono <- orderCells(FibroMono)
+## Plotting Figures ###############
 png(filename="Trajectory.png")
 plot_ordering_genes(FibroMono)
 dev.off()
@@ -199,9 +140,3 @@ plot_pseudotime_heatmap(FibroMono[my_genes,],
 dev.off()
 
 
-data(FibroMono)
-my_genes <- row.names(subset(fData(FibroMono), gene_short_name %in% c("Inmt", "Nr4a1", "Ccl19")))
-my_genes
-cds_subset <- FibroMono[my_genes,]
-cds_subset
-plot_genes_in_pseudotime(cds_subset, color_by="updated")
